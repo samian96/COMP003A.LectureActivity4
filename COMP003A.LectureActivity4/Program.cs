0833e153b5d63ee3a05dfbe408b10fef2ba4c79b
samian <sam.iannazzo@yahoo.com>
2026-02-02 18:58:49 -0800

    added writeline to line 9 to display section

--- COMP003A.LectureActivity4/Program.cs
+++ COMP003A.LectureActivity4/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             int taskNumber = 1;
+
+            Console.WriteLine("Section A: While Loop Practice")
             // while loop going through numbers 1 to 5 to display task that has been completed
             while (taskNumber <= 5)
             {
